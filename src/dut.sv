e3db22d0cc5cd64046a6f235364b5c67df2b0404
Andreas Kuster <mail@andreaskuster.ch>
2022-01-25 21:28:48 +0100

    Fix formatting. Remove unused code. Fix last bugs in AXI to (req/resp) pair conversion. Add trace generation

--- src/dut.sv
+++ src/dut.sv
@@ -9,13 +9,9 @@
 // specific language governing permissions and limitations under the License.
 //
 // Author:      Andreas Kuster, <kustera@ethz.ch>
-// Description: Design under test
-
+// Description: Design under test (AXI IO-PMP)
 
 `timescale 1ns / 1ps
-
-// `include "axi_conf.sv"
-
 
 module dut #(
     // Width of data bus in bits
@@ -46,17 +42,17 @@ module dut #(
     parameter RUSER_ENABLE  = 0,
     // Width of ruser signal
     parameter RUSER_WIDTH   = 1,
-    // register type { Bypass = 0, Registered = 1, Skid Buffer = 2}
+    // register type { Bypass = 0, Registered = 1, Skid Buffer = 2 }
     parameter REG_TYPE      = 1,
     // Waveform generation { Off=0, On=1 }
     parameter WAVES         = 0
 ) (
-    input  wire                     clk,
-    input  wire                     rst,
-
+    input  wire                clk,
+    input  wire                rst,
     /*
      * AXI master interface
      */
+    // Write address channel
     output [ID_WIDTH-1:0]      m_axi_awid,
     output [ADDR_WIDTH-1:0]    m_axi_awaddr,
     output [7:0]               m_axi_awlen,
@@ -70,17 +66,20 @@ module dut #(
     output [AWUSER_WIDTH-1:0]  m_axi_awuser,
     output                     m_axi_awvalid,
     input                      m_axi_awready,
+    // Write data channel
     output [DATA_WIDTH-1:0]    m_axi_wdata,
     output [STRB_WIDTH-1:0]    m_axi_wstrb,
     output                     m_axi_wlast,
     output [WUSER_WIDTH-1:0]   m_axi_wuser,
     output                     m_axi_wvalid,
     input                      m_axi_wready,
+    // Write response channel
     input  [ID_WIDTH-1:0]      m_axi_bid,
     input  [1:0]               m_axi_bresp,
     input  [BUSER_WIDTH-1:0]   m_axi_buser,
     input                      m_axi_bvalid,
     output                     m_axi_bready,
+    // Read address channel
     output [ID_WIDTH-1:0]      m_axi_arid,
     output [ADDR_WIDTH-1:0]    m_axi_araddr,
     output [7:0]               m_axi_arlen,
@@ -92,8 +91,9 @@ module dut #(
     output [3:0]               m_axi_arqos,
     output [3:0]               m_axi_arregion,
     output [ARUSER_WIDTH-1:0]  m_axi_aruser,
-    output                      m_axi_arvalid,
+    output                     m_axi_arvalid,
     input                      m_axi_arready,
+    // Read data channel
     input  [ID_WIDTH-1:0]      m_axi_rid,
     input  [DATA_WIDTH-1:0]    m_axi_rdata,
     input  [1:0]               m_axi_rresp,
@@ -101,10 +101,10 @@ module dut #(
     input  [RUSER_WIDTH-1:0]   m_axi_ruser,
     input                      m_axi_rvalid,
     output                     m_axi_rready,
-
     /*
      * AXI slave interface
      */
+    // Write address channel
     input [ID_WIDTH-1:0]     s_axi_awid,
     input [ADDR_WIDTH-1:0]   s_axi_awaddr,
     input [7:0]              s_axi_awlen,
@@ -118,17 +118,20 @@ module dut #(
     input [AWUSER_WIDTH-1:0] s_axi_awuser,
     input                    s_axi_awvalid,
     output                   s_axi_awready,
+    // Write data channel
     input [DATA_WIDTH-1:0]   s_axi_wdata,
     input [STRB_WIDTH-1:0]   s_axi_wstrb,
     input                    s_axi_wlast,
     input [WUSER_WIDTH-1:0]  s_axi_wuser,
     input                    s_axi_wvalid,
     output                   s_axi_wready,
+    // Write response channel
     output [ID_WIDTH-1:0]    s_axi_bid,
     output [1:0]             s_axi_bresp,
     output [BUSER_WIDTH-1:0] s_axi_buser,
     output                   s_axi_bvalid,
     input                    s_axi_bready,
+    // Read address channel
     input [ID_WIDTH-1:0]     s_axi_arid,
     input [ADDR_WIDTH-1:0]   s_axi_araddr,
     input [7:0]              s_axi_arlen,
@@ -142,6 +145,7 @@ module dut #(
     input [ARUSER_WIDTH-1:0] s_axi_aruser,
     input                    s_axi_arvalid,
     output                   s_axi_arready,
+    // Read data channel
     output [ID_WIDTH-1:0]    s_axi_rid,
     output [DATA_WIDTH-1:0]  s_axi_rdata,
     output [1:0]             s_axi_rresp,
@@ -151,251 +155,189 @@ module dut #(
     input                    s_axi_rready
 );
 
+    /*
+     * Master/slave request/response structs
+     */
+    axi_conf::req_t  m_axi_req_i,  s_axi_req_o;
+    axi_conf::resp_t m_axi_resp_o, s_axi_resp_i;
 
-    // AXI_BUS #(
-    // .AXI_ADDR_WIDTH(ADDR_WIDTH),
-    // .AXI_DATA_WIDTH(DATA_WIDTH),
-    // .AXI_ID_WIDTH(ID_WIDTH),
-    // .AXI_USER_WIDTH(BUSER_WIDTH)
-    // ) from_master(), to_slave();
+    /*
+     * Traditional AXI slave signal to (req/resp) pair conversion
+     */ 
+    axi_slave_connector #(
+        .DATA_WIDTH    ( DATA_WIDTH   ),
+        .ADDR_WIDTH    ( ADDR_WIDTH   ),
+        .STRB_WIDTH    ( STRB_WIDTH   ),
+        .ID_WIDTH      ( ID_WIDTH     ),
+        .AWUSER_WIDTH  ( AWUSER_WIDTH ),
+        .WUSER_WIDTH   ( WUSER_WIDTH  ),
+        .BUSER_WIDTH   ( BUSER_WIDTH  ),
+        .ARUSER_WIDTH  ( ARUSER_WIDTH ),
+        .RUSER_WIDTH   ( RUSER_WIDTH  )
+    ) slave_connector0 (
+        /*
+         *
+         */
+        .s_axi_awid    ( s_axi_awid     ),
+        .s_axi_awaddr  ( s_axi_awaddr   ),
+        .s_axi_awlen   ( s_axi_awlen    ),
+        .s_axi_awsize  ( s_axi_awsize   ),
+        .s_axi_awburst ( s_axi_awburst  ),
+        .s_axi_awlock  ( s_axi_awlock   ),
+        .s_axi_awcache ( s_axi_awcache  ),
+        .s_axi_awprot  ( s_axi_awprot   ),
+        .s_axi_awqos   ( s_axi_awqos    ),
+        .s_axi_awregion( s_axi_awregion ),
+        .s_axi_awuser  ( s_axi_awuser   ),
+        .s_axi_awvalid ( s_axi_awvalid  ),
+        .s_axi_awready ( s_axi_awready  ),
+        .s_axi_wdata   ( s_axi_wdata    ),
+        .s_axi_wstrb   ( s_axi_wstrb    ),
+        .s_axi_wlast   ( s_axi_wlast    ),
+        .s_axi_wuser   ( s_axi_wuser    ),
+        .s_axi_wvalid  ( s_axi_wvalid   ),
+        .s_axi_wready  ( s_axi_wready   ),
+        .s_axi_bid     ( s_axi_bid      ),
+        .s_axi_bresp   ( s_axi_bresp    ),
+        .s_axi_buser   ( s_axi_buser    ),
+        .s_axi_bvalid  ( s_axi_bvalid   ),
+        .s_axi_bready  ( s_axi_bready   ),
+        .s_axi_arid    ( s_axi_arid     ),
+        .s_axi_araddr  ( s_axi_araddr   ),
+        .s_axi_arlen   ( s_axi_arlen    ),
+        .s_axi_arsize  ( s_axi_arsize   ),
+        .s_axi_arburst ( s_axi_arburst  ),
+        .s_axi_arlock  ( s_axi_arlock   ),
+        .s_axi_arcache ( s_axi_arcache  ),
+        .s_axi_arprot  ( s_axi_arprot   ),
+        .s_axi_arqos   ( s_axi_arqos    ),
+        .s_axi_arregion( s_axi_arregion ),
+        .s_axi_aruser  ( s_axi_aruser   ),
+        .s_axi_arvalid ( s_axi_arvalid  ),
+        .s_axi_arready ( s_axi_arready  ),
+        .s_axi_rid     ( s_axi_rid      ),
+        .s_axi_rdata   ( s_axi_rdata    ),
+        .s_axi_rresp   ( s_axi_rresp    ),
+        .s_axi_rlast   ( s_axi_rlast    ),
+        .s_axi_ruser   ( s_axi_ruser    ),
+        .s_axi_rvalid  ( s_axi_rvalid   ),
+        .s_axi_rready  ( s_axi_rready   ),
+        /*
+         * AXI request/response pair
+         */
+        .axi_req_o     ( s_axi_req_o    ),
+        .axi_resp_i    ( s_axi_resp_i   )
+    );
+
+    /*
+     * Traditional AXI master signal to (req/resp) pair conversion
+     */ 
+    axi_master_connector #(
+        .DATA_WIDTH  ( DATA_WIDTH   ),
+        .ADDR_WIDTH  ( ADDR_WIDTH   ),
+        .STRB_WIDTH  ( STRB_WIDTH   ),
+        .ID_WIDTH    ( ID_WIDTH     ),
+        .AWUSER_WIDTH( AWUSER_WIDTH ),
+        .WUSER_WIDTH ( WUSER_WIDTH  ),
+        .BUSER_WIDTH ( BUSER_WIDTH  ),
+        .ARUSER_WIDTH( ARUSER_WIDTH ),
+        .RUSER_WIDTH ( RUSER_WIDTH  )
+    ) master_connector0 (
+        /*
+         *
+         */
+        .m_axi_awid    ( m_axi_awid     ),
+        .m_axi_awaddr  ( m_axi_awaddr   ),
+        .m_axi_awlen   ( m_axi_awlen    ),
+        .m_axi_awsize  ( m_axi_awsize   ),
+        .m_axi_awburst ( m_axi_awburst  ),
+        .m_axi_awlock  ( m_axi_awlock   ),
+        .m_axi_awcache ( m_axi_awcache  ),
+        .m_axi_awprot  ( m_axi_awprot   ),
+        .m_axi_awqos   ( m_axi_awqos    ),
+        .m_axi_awregion( m_axi_awregion ),
+        .m_axi_awuser  ( m_axi_awuser   ),
+        .m_axi_awvalid ( m_axi_awvalid  ),
+        .m_axi_awready ( m_axi_awready  ),
+        .m_axi_wdata   ( m_axi_wdata    ),
+        .m_axi_wstrb   ( m_axi_wstrb    ),
+        .m_axi_wlast   ( m_axi_wlast    ),
+        .m_axi_wuser   ( m_axi_wuser    ),
+        .m_axi_wvalid  ( m_axi_wvalid   ),
+        .m_axi_wready  ( m_axi_wready   ),
+        .m_axi_bid     ( m_axi_bid      ),
+        .m_axi_bresp   ( m_axi_bresp    ),
+        .m_axi_buser   ( m_axi_buser    ),
+        .m_axi_bvalid  ( m_axi_bvalid   ),
+        .m_axi_bready  ( m_axi_bready   ),
+        .m_axi_arid    ( m_axi_arid     ),
+        .m_axi_araddr  ( m_axi_araddr   ),
+        .m_axi_arlen   ( m_axi_arlen    ),
+        .m_axi_arsize  ( m_axi_arsize   ),
+        .m_axi_arburst ( m_axi_arburst  ),
+        .m_axi_arlock  ( m_axi_arlock   ),
+        .m_axi_arcache ( m_axi_arcache  ),
+        .m_axi_arprot  ( m_axi_arprot   ),
+        .m_axi_arqos   ( m_axi_arqos    ),
+        .m_axi_arregion( m_axi_arregion ),
+        .m_axi_aruser  ( m_axi_aruser   ),
+        .m_axi_arvalid ( m_axi_arvalid  ),
+        .m_axi_arready ( m_axi_arready  ),
+        .m_axi_rid     ( m_axi_rid      ),
+        .m_axi_rdata   ( m_axi_rdata    ),
+        .m_axi_rresp   ( m_axi_rresp    ),
+        .m_axi_rlast   ( m_axi_rlast    ),
+        .m_axi_ruser   ( m_axi_ruser    ),
+        .m_axi_rvalid  ( m_axi_rvalid   ),
+        .m_axi_rready  ( m_axi_rready   ),
+        /*
+         * AXI request/response pair
+         */
+        .axi_req_i(m_axi_req_i),
+        .axi_resp_o(m_axi_resp_o)
+    );
+
+    /*
+     * Device under test, AXI IO-PMP
+     */ 
+    axi_io_pmp #(
+        .DATA_WIDTH   ( DATA_WIDTH    ),
+        .ADDR_WIDTH   ( ADDR_WIDTH    ),
+        .STRB_WIDTH   ( STRB_WIDTH    ),
+        .ID_WIDTH     ( ID_WIDTH      ),
+        .AWUSER_ENABLE( AWUSER_ENABLE ),
+        .AWUSER_WIDTH ( AWUSER_WIDTH  ),
+        .WUSER_ENABLE ( WUSER_ENABLE  ),
+        .WUSER_WIDTH  ( WUSER_WIDTH   ),
+        .BUSER_ENABLE ( BUSER_ENABLE  ),
+        .BUSER_WIDTH  ( BUSER_WIDTH   ),
+        .ARUSER_ENABLE( ARUSER_ENABLE ),
+        .ARUSER_WIDTH ( ARUSER_WIDTH  ),
+        .RUSER_ENABLE ( RUSER_ENABLE  ),
+        .RUSER_WIDTH  ( RUSER_WIDTH   ),
+        .REG_TYPE     ( REG_TYPE      ),
+        .WAVES        ( WAVES         )
+    ) axi_io_pmp0 (
+        .clk       ( clk          ),
+        .rst       ( rst          ),
+        .slv_req_i ( s_axi_req_o  ),
+        .slv_resp_o( s_axi_resp_i ), 
+        .mst_req_o ( m_axi_req_i  ),
+        .mst_resp_i( m_axi_resp_o )
+    );
 
 
-    // /*
-    // * Write address channel
-    // */
-    // assign m_axi_awid          = from_master.Slave.aw_id;
-    // assign m_axi_awaddr        = from_master.Slave.aw_addr;
-    // assign m_axi_awlen         = from_master.Slave.aw_len;
-    // assign m_axi_awsize        = from_master.Slave.aw_size;
-    // assign m_axi_awburst       = from_master.Slave.aw_burst;
-    // assign m_axi_awlock        = from_master.Slave.aw_lock;
-    // assign m_axi_awcache       = from_master.Slave.aw_cache;
-    // assign m_axi_awprot        = from_master.Slave.aw_prot;
-    // assign m_axi_awqos         = from_master.Slave.aw_qos;
-    // // assign master.aw_atop      = from_master.Master.aw.atop; // TODO: check if we should/can add this field to the axi interface
-    // assign m_axi_awregion      = from_master.Slave.aw_region;
-    // assign m_axi_awuser        = from_master.Slave.aw_user;
-    // assign m_axi_awvalid       = from_master.Slave.aw_valid;
-    // assign from_master.Slave.aw_ready = m_axi_awready;
-
-
-    // /*
-    //     * Write data channel
-    //     */
-    // assign m_axi_wdata        = from_master.Master.w_data;
-    // assign m_axi_wstrb        = from_master.Master.w_strb;
-    // assign m_axi_wlast        = from_master.Master.w_last;
-    // assign m_axi_wuser        = from_master.Master.w_user;
-    // assign m_axi_wvalid       = from_master.Master.w_valid;
-    // assign from_master.Master.w_ready = m_axi_wready;
-
-    // /*
-    //  * Write response channel
-    //  */
-    // assign from_master.Master.b_id    = m_axi_bid;
-    // assign from_master.Master.b_resp  = m_axi_bresp;
-    // assign from_master.Master.b_user  = m_axi_buser;
-    // assign from_master.Master.b_valid = m_axi_bvalid;
-    // assign m_axi_bready       = from_master.Master.b_ready;
-
-    // /*
-    //  * Read address channel
-    //  */
-    // assign m_axi_arid          = from_master.Master.ar_id;
-    // assign m_axi_araddr        = from_master.Master.ar_addr;
-    // assign m_axi_arlen         = from_master.Master.ar_len;
-    // assign m_axi_arsize        = from_master.Master.ar_size;
-    // assign m_axi_arburst       = from_master.Master.ar_burst;
-    // assign m_axi_arlock        = from_master.Master.ar_lock;
-    // assign m_axi_arcache       = from_master.Master.ar_cache;
-    // assign m_axi_arprot        = from_master.Master.ar_prot;
-    // assign m_axi_arqos         = from_master.Master.ar_qos;
-    // assign m_axi_arregion      = from_master.Master.ar_region;
-    // assign m_axi_aruser        = from_master.Master.ar_user;
-    // assign m_axi_arvalid       = from_master.Master.ar_valid;
-    // assign from_master.Master.ar_ready = m_axi_arready;
-
-    // /*
-    //  * Read data channel
-    //  */
-    // assign from_master.Master.r_id    = m_axi_rid;
-    // assign from_master.Master.r_data  = m_axi_rdata;
-    // assign from_master.Master.r_resp  = m_axi_rresp;
-    // assign from_master.Master.r_last  = m_axi_rlast;
-    // assign from_master.Master.r_user  = m_axi_ruser;
-    // assign from_master.Master.r_valid = m_axi_rvalid;
-    // assign m_axi_rready       = from_master.Master.r_ready;
-
-
-
-//   modport Master (
-//     output aw_id, aw_addr, aw_len, aw_size, aw_burst, aw_lock, aw_cache, aw_prot, aw_qos, aw_region, aw_atop, aw_user, aw_valid, input aw_ready,
-//     output w_data, w_strb, w_last, w_user, w_valid, input w_ready,
-//     input b_id, b_resp, b_user, b_valid, output b_ready,
-//     output ar_id, ar_addr, ar_len, ar_size, ar_burst, ar_lock, ar_cache, ar_prot, ar_qos, ar_region, ar_user, ar_valid, input ar_ready,
-//     input r_id, r_data, r_resp, r_last, r_user, r_valid, output r_ready
-//   );
-
-//   modport Slave (
-//     input aw_id, aw_addr, aw_len, aw_size, aw_burst, aw_lock, aw_cache, aw_prot, aw_qos, aw_region, aw_atop, aw_user, aw_valid, output aw_ready,
-//     input w_data, w_strb, w_last, w_user, w_valid, output w_ready,
-//     output b_id, b_resp, b_user, b_valid, input b_ready,
-//     input ar_id, ar_addr, ar_len, ar_size, ar_burst, ar_lock, ar_cache, ar_prot, ar_qos, ar_region, ar_user, ar_valid, output ar_ready,
-//     output r_id, r_data, r_resp, r_last, r_user, r_valid, input r_ready
-//   );
-
-
-axi_conf::req_t m_axi_req_i, s_axi_req_o;
-axi_conf::resp_t m_axi_resp_o, s_axi_resp_i;
-
-
-
-axi_slave_connector #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .ADDR_WIDTH(ADDR_WIDTH),
-    .STRB_WIDTH(STRB_WIDTH),
-    .ID_WIDTH(ID_WIDTH),
-    .AWUSER_WIDTH(AWUSER_WIDTH),
-    .WUSER_WIDTH(WUSER_WIDTH),
-    .BUSER_WIDTH(BUSER_WIDTH),
-    .ARUSER_WIDTH(ARUSER_WIDTH),
-    .RUSER_WIDTH(RUSER_WIDTH)
-) slave_connector0 (
-    .s_axi_awid(s_axi_awid),
-    .s_axi_awaddr(s_axi_awaddr),
-    .s_axi_awlen(s_axi_awlen),
-    .s_axi_awsize(s_axi_awsize),
-    .s_axi_awburst(s_axi_awburst),
-    .s_axi_awlock(s_axi_awlock),
-    .s_axi_awcache(s_axi_awcache),
-    .s_axi_awprot(s_axi_awprot),
-    .s_axi_awqos(s_axi_awqos),
-    .s_axi_awregion(s_axi_awregion),
-    .s_axi_awuser(s_axi_awuser),
-    .s_axi_awvalid(s_axi_awvalid),
-    .s_axi_awready(s_axi_awready),
-    .s_axi_wdata(s_axi_wdata),
-    .s_axi_wstrb(s_axi_wstrb),
-    .s_axi_wlast(s_axi_wlast),
-    .s_axi_wuser(s_axi_wuser),
-    .s_axi_wvalid(s_axi_wvalid),
-    .s_axi_wready(s_axi_wready),
-    .s_axi_bid(s_axi_bid),
-    .s_axi_bresp(s_axi_bresp),
-    .s_axi_buser(s_axi_buser),
-    .s_axi_bvalid(s_axi_bvalid),
-    .s_axi_bready(s_axi_bready),
-    .s_axi_arid(s_axi_arid),
-    .s_axi_araddr(s_axi_araddr),
-    .s_axi_arlen(s_axi_arlen),
-    .s_axi_arsize(s_axi_arsize),
-    .s_axi_arburst(s_axi_arburst),
-    .s_axi_arlock(s_axi_arlock),
-    .s_axi_arcache(s_axi_arcache),
-    .s_axi_arprot(s_axi_arprot),
-    .s_axi_arqos(s_axi_arqos),
-    .s_axi_arregion(s_axi_arregion),
-    .s_axi_aruser(s_axi_aruser),
-    .s_axi_arvalid(s_axi_arvalid),
-    .s_axi_arready(s_axi_arready),
-    .s_axi_rid(s_axi_rid),
-    .s_axi_rdata(s_axi_rdata),
-    .s_axi_rresp(s_axi_rresp),
-    .s_axi_rlast(s_axi_rlast),
-    .s_axi_ruser(s_axi_ruser),
-    .s_axi_rvalid(s_axi_rvalid),
-    .s_axi_rready(s_axi_rready),
-    
-    .axi_req_o(s_axi_req_o),
-    .axi_resp_i(s_axi_resp_i)
-);
-
-
-axi_master_connector #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .ADDR_WIDTH(ADDR_WIDTH),
-    .STRB_WIDTH(STRB_WIDTH),
-    .ID_WIDTH(ID_WIDTH),
-    .AWUSER_WIDTH(AWUSER_WIDTH),
-    .WUSER_WIDTH(WUSER_WIDTH),
-    .BUSER_WIDTH(BUSER_WIDTH),
-    .ARUSER_WIDTH(ARUSER_WIDTH),
-    .RUSER_WIDTH(RUSER_WIDTH)
-) master_connector0 (
-    .m_axi_awid(m_axi_awid),
-    .m_axi_awaddr(m_axi_awaddr),
-    .m_axi_awlen(m_axi_awlen),
-    .m_axi_awsize(m_axi_awsize),
-    .m_axi_awburst(m_axi_awburst),
-    .m_axi_awlock(m_axi_awlock),
-    .m_axi_awcache(m_axi_awcache),
-    .m_axi_awprot(m_axi_awprot),
-    .m_axi_awqos(m_axi_awqos),
-    .m_axi_awregion(m_axi_awregion),
-    .m_axi_awuser(m_axi_awuser),
-    .m_axi_awvalid(m_axi_awvalid),
-    .m_axi_awready(m_axi_awready),
-    .m_axi_wdata(m_axi_wdata),
-    .m_axi_wstrb(m_axi_wstrb),
-    .m_axi_wlast(m_axi_wlast),
-    .m_axi_wuser(m_axi_wuser),
-    .m_axi_wvalid(m_axi_wvalid),
-    .m_axi_wready(m_axi_wready),
-    .m_axi_bid(m_axi_bid),
-    .m_axi_bresp(m_axi_bresp),
-    .m_axi_buser(m_axi_buser),
-    .m_axi_bvalid(m_axi_bvalid),
-    .m_axi_bready(m_axi_bready),
-    .m_axi_arid(m_axi_arid),
-    .m_axi_araddr(m_axi_araddr),
-    .m_axi_arlen(m_axi_arlen),
-    .m_axi_arsize(m_axi_arsize),
-    .m_axi_arburst(m_axi_arburst),
-    .m_axi_arlock(m_axi_arlock),
-    .m_axi_arcache(m_axi_arcache),
-    .m_axi_arprot(m_axi_arprot),
-    .m_axi_arqos(m_axi_arqos),
-    .m_axi_arregion(m_axi_arregion),
-    .m_axi_aruser(m_axi_aruser),
-    .m_axi_arvalid(m_axi_arvalid),
-    .m_axi_arready(m_axi_arready),
-    .m_axi_rid(m_axi_rid),
-    .m_axi_rdata(m_axi_rdata),
-    .m_axi_rresp(m_axi_rresp),
-    .m_axi_rlast(m_axi_rlast),
-    .m_axi_ruser(m_axi_ruser),
-    .m_axi_rvalid(m_axi_rvalid),
-    .m_axi_rready(m_axi_rready),
-
-    .axi_req_i(m_axi_req_i),
-    .axi_resp_o(m_axi_resp_o)
-);
-
-axi_io_pmp #(
-    .DATA_WIDTH(DATA_WIDTH),
-    .ADDR_WIDTH(ADDR_WIDTH),
-    .STRB_WIDTH(STRB_WIDTH),
-    .ID_WIDTH(ID_WIDTH),
-    .AWUSER_ENABLE(AWUSER_ENABLE),
-    .AWUSER_WIDTH(AWUSER_WIDTH),
-    .WUSER_ENABLE(WUSER_ENABLE),
-    .WUSER_WIDTH(WUSER_WIDTH),
-    .BUSER_ENABLE(BUSER_ENABLE),
-    .BUSER_WIDTH(BUSER_WIDTH),
-    .ARUSER_ENABLE(ARUSER_ENABLE),
-    .ARUSER_WIDTH(ARUSER_WIDTH),
-    .RUSER_ENABLE(RUSER_ENABLE),
-    .RUSER_WIDTH(RUSER_WIDTH),
-    .REG_TYPE(REG_TYPE),
-    .WAVES(WAVES)
-) axi_io_pmp0 (
-    .clk(clk),
-    .rst(clk),
-    .slv_req_i(s_axi_req_o),
-    .slv_resp_o(s_axi_resp_i), 
-    .mst_req_o(m_axi_req_i),
-    .mst_resp_i(m_axi_resp_o)
-);
-
+    /*
+     * Simulation / Debugging
+     */
+   `ifdef COCOTB_SIM
+    initial begin
+        if(WAVES == 1) begin
+            // Generate trace file
+            $dumpfile("axi_io_pmp.vcd");
+            $dumpvars(0, dut);
+        end
+    end
+    `endif
 
 endmodule
