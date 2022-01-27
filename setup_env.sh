3fd2171385298c0dbc0b399ec5a32447c7a9b10a
Andreas Kuster <mail@andreaskuster.ch>
2022-01-27 13:08:59 +0100

    Add license header

--- setup_env.sh
+++ setup_env.sh
@@ -1,5 +1,20 @@
 #!/usr/bin/env bash
 
+# Copyright 2022 ETH Zurich and University of Bologna.
+# Copyright and related rights are licensed under the Solderpad Hardware
+# License, Version 0.51 (the "License"); you may not use this file except in
+# compliance with the License.  You may obtain a copy of the License at
+# http://solderpad.org/licenses/SHL-0.51. Unless required by applicable law
+# or agreed to in writing, software, hardware and materials distributed under
+# this License is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
+# CONDITIONS OF ANY KIND, either express or implied. See the License for the
+# specific language governing permissions and limitations under the License.
+#
+# Author:      Andreas Kuster, <kustera@ethz.ch>
+# Description: Setup linux and python (virtual) environment, including relevant
+#              environment variables and missing dependencies.
+
+
 # add eda tools packcage repo
 echo 'deb http://download.opensuse.org/repositories/home:/phiwag:/edatools/xUbuntu_20.04/ /' | sudo tee /etc/apt/sources.list.d/home:phiwag:edatools.list
 curl -fsSL https://download.opensuse.org/repositories/home:phiwag:edatools/xUbuntu_20.04/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/home_phiwag_edatools.gpg > /dev/null
