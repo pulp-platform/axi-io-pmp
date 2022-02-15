173c937dd03b4c147b58693faf0b9a0c3fac4d95
Andreas Kuster <mail@andreaskuster.ch>
2022-02-15 18:55:52 +0100

    Add c/cpp code formatting and initial SystemVerilog formatting

---
+++ extras/format.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# clang formatting
+echo "Format c/cpp/h files"
+find . -not -path "./.*" -name "*.cpp" -o -name "*.c" -o -name "*.h" | xargs -I {} clang-format -i {}
+
+# todo: add verible verilog formatter
+echo "Install verible.."
+wget https://github.com/chipsalliance/verible/releases/download/v0.0-1897-gbe7e2250/verible-v0.0-1897-gbe7e2250-Ubuntu-20.04-focal-x86_64.tar.gz -O verible.tar.gz
+tar -xzf verible.tar.gz
+export PATH=./verible-v0.0-1897-gbe7e2250/bin/:$PATH
+
+echo "Format verilog files.."
+
+
+echo "All done."
+
