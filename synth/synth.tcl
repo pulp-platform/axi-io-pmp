47488afce4d2cd0d085f7e357c9469bf173826b7
Andreas Kuster <mail@andreaskuster.ch>
2022-02-19 15:36:10 +0100

    Set satisfiable constraints

--- synth/synth.tcl
+++ synth/synth.tcl
@@ -2,23 +2,27 @@
 sh rm -rf WORK/*
 remove_design -design
 
-# set multithreading
+# enable multithreading
 set disable_multicore_resource_checks true
 set NumberThreads [exec cat /proc/cpuinfo | grep -c processor]
 set_host_options -max_cores $NumberThreads
 
+# import & analyze source files
 source analyze.tcl
 
+# elaborate design from top level
 elaborate dut
 
-# Constraints
-set_max_delay -to [all_outputs] 400
+# set constraints
+set_max_delay -to [all_outputs] 500
 set_max_area 0
 set_load 0.1 [all_outputs]
 set_max_fanout 1 [all_inputs]
 set_fanout_load 8 [all_outputs]
 
-#compile_ultra
-compile_ultra -gate_clock -no_autoungroup -incremental
+# do the actual synthesis
+compile_ultra -incremental
 
+# generate reports
 report_timing
+report_area
