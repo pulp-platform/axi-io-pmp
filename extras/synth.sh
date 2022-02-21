49df343169a5fc0c9ff8ece6cd726fd6d8ca09b1
Andreas Kuster <mail@andreaskuster.ch>
2022-02-21 16:37:53 +0100

    Add kGE compute script

--- extras/synth.sh
+++ extras/synth.sh
@@ -4,3 +4,5 @@ cd OpenROAD-flow-scripts/
 source ./setup_env.sh
 cd flow/
 make DESIGN_CONFIG=../../config.mk
+
+# python3 getKGE.py OpenROAD-flow-scripts/flow/platforms/nangate45/lib/NangateOpenCellLibrary_typical.lib OpenROAD-flow-scripts/flow/reports/
