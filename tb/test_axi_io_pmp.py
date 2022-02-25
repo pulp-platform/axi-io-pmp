a85c66f7f67643dac7c920b0fb85ab3b9c53567e
Andreas Kuster <mail@andreaskuster.ch>
2022-02-25 14:25:44 +0100

    Add extensive tests (denial/access for all length and burst sizes, read/write, granularity check, boundary checks and all individual PMP slot checks)

--- tb/test_axi_io_pmp.py
+++ tb/test_axi_io_pmp.py
@@ -9,7 +9,7 @@ import cocotb_test.simulator
 from cocotb.clock import Clock
 from cocotb.regression import TestFactory
 from cocotb.triggers import RisingEdge
-from cocotbext.axi import AxiBus, AxiMaster, AxiRam, AxiSlave
+from cocotbext.axi import AxiBus, AxiMaster, AxiRam, AxiSlave, AxiResp
 
 import numpy as np
 
@@ -17,6 +17,8 @@ import math
 from enum import Enum
 from bitarray import bitarray
 from bitarray.util import int2ba, ba2int
+import itertools
+import random
 
 
 class TB:
@@ -37,6 +39,7 @@ class TB:
 
         # connect simulation axi PMP config
         self.axi_pmp_cfg = AxiMaster(AxiBus.from_prefix(dut, "cfg_axi"), dut.clk, dut.rst)
+
 
     async def cycle_reset(self):
         self.dut.rst.setimmediatevalue(0)
@@ -82,7 +85,13 @@ def get_addr_offset():
     return params
 
 
-async def set_pmp_napot(tb, base: int, range: int, access: bitarray, pmp_no: int, PMP_LEN=54):
+async def set_pmp_napot(tb, base: int, length: int, access: bitarray, pmp_no: int, PMP_LEN=54):
+    """
+    TODO
+    """
+    if length < 2 ** 12:
+        tb.log.info(f"set_pmp_napot: length is below granularity level, range is extended to 2**12")
+
     # read base addresses (addr & cfg from header)
     params = get_addr_offset()
 
@@ -94,18 +103,15 @@ async def set_pmp_napot(tb, base: int, range: int, access: bitarray, pmp_no: int
     tb.log.info("PMP cfg: %s", conf.to01())
     # address
     PMP_LEN = tb.dut.axi_io_pmp0.PMP_LEN.value
-    napot_addr = int(base + (range / 2 - 1)) >> 2
+    napot_addr = int(base + (length / 2 - 1)) >> 2
     tb.log.info("PMP NAPOT addr: %s", int2ba(napot_addr, PMP_LEN).to01())
 
     # write config
     pmp0_addr = params["IO_PMP_PMP_ADDR_0_REG_OFFSET"] + 8 * pmp_no
-    res = await tb.axi_pmp_cfg.read(pmp0_addr + 8, 8)
-    pmp_next = int.from_bytes(res.data,
-                              byteorder="little")  # workaround: we can only write  >= 16bytes (axi_to_reg issue), therefore we read address above first, and write back both
-    await tb.axi_pmp_cfg.write(pmp0_addr, (napot_addr + (pmp_next * (2 ** 64))).to_bytes(16, byteorder='little'))
+    await tb.axi_pmp_cfg.write(pmp0_addr, (napot_addr).to_bytes(8, byteorder='little'))
 
     pmp0_cfg = params["IO_PMP_PMP_CFG_0_REG_OFFSET"]
-    res = await tb.axi_pmp_cfg.read(pmp0_cfg, 16)
+    res = await tb.axi_pmp_cfg.read(pmp0_cfg, 8)
     pmp_cfg_data = int.from_bytes(res.data, byteorder="little")
 
     # remove old value
@@ -118,115 +124,247 @@ async def set_pmp_napot(tb, base: int, range: int, access: bitarray, pmp_no: int
     await tb.axi_pmp_cfg.write(pmp0_cfg, (pmp_cfg_data).to_bytes(16, byteorder='little'))
 
 
-async def run_test(dut):
-    # get testbed instance
+async def run_test_write_read(dut, length: int, size: int, block: bool, mem_region=(0, 2 ** 16), pmp_slot=0):
+    """
+    Allow the full AxiRAM range (0 .. 2**16 -1), and run extensive tests (different burst length & offsets) to check if all transactions
+    succeed (Note: boundary crossings would still cause a AxiResp.SLVERR, however the AxiMaster module splits them)
+    """
+    # get testbed instance & reset
     tb = TB(dut)
-
-    # reset dut
     await tb.cycle_reset()
+
+    # allow/block all PMP, we check for that
+    if block:  # block access
+        await set_pmp_napot(tb, mem_region[0], mem_region[1], PMPAccess.ACCESS_NONE.value, pmp_slot)
+    else:
+        await set_pmp_napot(tb, mem_region[0], mem_region[1],
+                            PMPAccess.ACCESS_READ.value | PMPAccess.ACCESS_WRITE.value, pmp_slot)
+
+    # run extensive AXI write-then-read tests
+    addresses = [mem_region[0], mem_region[1] - length] + [random.randint(0, 2 ** 16 - length) for _ in range(2)]
+    for base_addr in addresses:  # test boundaries + 2 random addresses (in the valid range)
+
+        # prepare
+        tb.log.info("AXI transaction info: length %d, base_addr %d, size %d", length, base_addr, size)
+        test_data = bytearray([x % 256 for x in range(length)])
+
+        # initiate write transfer
+        if os.environ["SIM"] == "icarus" and block:  # workaround for icarus: blocks on AxiResp.SLVERR write: write to AxiRAM directly instead
+            if base_addr < 2 ** 16:
+                tb.axi_ram.write(base_addr, test_data)
+        else:
+            res = await tb.axi_master.write(base_addr, test_data, size=size)
+            if block:
+                assert res.resp == AxiResp.SLVERR
+            else:
+                assert res.resp == AxiResp.OKAY
+
+        # initiate read transfer
+        data = await tb.axi_master.read(base_addr, length, size=size)
+
+        # check result
+        if block:
+            assert data.data != test_data
+            assert data.resp == AxiResp.SLVERR
+        else:
+            assert data.data == test_data
+            assert data.resp == AxiResp.OKAY
+
+    await RisingEdge(dut.clk)
+    await RisingEdge(dut.clk)
+
+
+async def run_test_bounds(dut, base: int = 0, length: int = 2 ** 12):
+    """
+     Test the PMP address boundaries for perfect match
+    """
+    # get testbed instance & reset
+    tb = TB(dut)
+    await tb.cycle_reset()
+
+    # prepare data
+    test_len = 16
+    test_data = bytearray([x % 256 for x in range(test_len)])
+
 
     ##################
     # write data to RAM (in order to have a deterministic value to read)
     ##################
-    addr = 0x0000_0000  # allowed range with address below: 0000_0000 - 0000_000f
-    length = 4
-    test_data = bytearray([x % 2 ** 8 for x in range(length)])
-    tb.log.info("TEST: addr %d, length %d, data %s", addr, length, test_data.hex())  # ("_", 1))
-    tb.axi_ram.write(addr, test_data)
+    tb.log.info("TEST: addr %d, length %d, data %s", base, test_len, test_data.hex())
+    tb.axi_ram.write(base, test_data)
 
     ###################
     # setup pmp entry
     ###################
-
-    # extract granularity according to RISC-V privileged specs
-    #
-    locked = bitarray("0")
-    reserved = bitarray("00")
-    mode = PMPMode.OFF.value
-    access = PMPAccess.ACCESS_NONE.value
-    conf: bitarray = locked + reserved + mode + access
-
-    await tb.axi_pmp_cfg.write(16*8, ba2int(conf).to_bytes(16, byteorder="little"))
-    await tb.axi_pmp_cfg.write(0, ((2**128) -1).to_bytes(16, byteorder="little"))
-
-    res = await tb.axi_pmp_cfg.read(0, 8)
-
-    g = 0
-    while(int.from_bytes(res.data, byteorder="little") & (0x1 << g) == 0):
-        g = g + 1
-    g = g + 2
-    tb.log.info(f"RETURN VALUE FOR GRANULARITY: {g}")
-
-
-    # configuration
-    pmp_range_len = 8  # 2**12
-    await set_pmp_napot(tb, 0, pmp_range_len, PMPAccess.ACCESS_READ.value | PMPAccess.ACCESS_WRITE.value, 0)
+    # configuration: allow first 4KB interval
+    await set_pmp_napot(tb, base, length, PMPAccess.ACCESS_READ.value | PMPAccess.ACCESS_WRITE.value, 0)
 
     ##########################
-    # read data through the IO-PMP
+    # read data through the IO-PMP (at base address)
     ###########################
-    data = await tb.axi_master.read(addr, length)
+    data = await tb.axi_master.read(base, test_len)
     tb.log.info("PMP read allow: %s", dut.axi_io_pmp0.pmp0.allow_o.value)
     tb.log.info("PMP write allow: %s", dut.axi_io_pmp0.pmp1.allow_o.value)
-
-    ###################
-    # check result
-    ###################
+    assert data.resp == AxiResp.OKAY
     assert data.data == test_data
 
     #####
     # check highest valid addr
     #####
-    test = 4 * 1024 - 4  # highest address that is still valid for 4byte read
-    await tb.axi_master.write(addr + test, test_data)
-    data = await tb.axi_master.read(addr + test, length)
+    addr = base + length - test_len
+    res = await tb.axi_master.write(addr, test_data)  # highest address that is still valid for 4byte read
+    data = await tb.axi_master.read(addr, test_len)
 
     tb.log.info("PMP read allow: %s", dut.axi_io_pmp0.pmp0.allow_o.value)
     tb.log.info("PMP write allow: %s", dut.axi_io_pmp0.pmp1.allow_o.value)
 
-    assert (dut.axi_io_pmp0.pmp0.allow_o.value == 1)
-    assert (dut.axi_io_pmp0.pmp1.allow_o.value == 1)
+    assert res.resp == AxiResp.OKAY
+    assert data.resp == AxiResp.OKAY
+    assert data.data == test_data
 
     #####
     # check lowest invalid addr
     #####
-    test = 4 * 1024  # lowest address that is out of PMP region
-    # await tb.axi_master.write(addr + test, test_data)
-    data = await tb.axi_master.read(addr + test, length)
+    addr = base + (length)
+    if os.environ["SIM"] == "icarus":  # workaround for icarus: blocks on AxiResp.SLVERR write: write to AxiRAM directly instead
+        if addr < 2**15:
+            tb.axi_ram.write(addr, test_data)
+    else:
+        res = await tb.axi_master.write(addr, test_data)
+        assert res.resp == AxiResp.SLVERR
 
-    tb.log.info("PMP read allow: %s", dut.axi_io_pmp0.pmp0.allow_o.value)
-    tb.log.info("PMP write allow: %s", dut.axi_io_pmp0.pmp1.allow_o.value)
-
-    assert (dut.axi_io_pmp0.pmp0.allow_o.value == 0)
-    # assert (dut.axi_io_pmp0.pmp1.allow_o.value == 0)
+    data = await tb.axi_master.read(addr, test_len)
+    assert data.resp == AxiResp.SLVERR
+    assert data.data != test_data
 
     #####
     # check boundary crossing
     #####
-    test = 4 * 1024 - 8
-    # await tb.axi_master.write(addr + test, (42).to_bytes(16, byteorder="little"))
-    data = await tb.axi_master.read(addr + test, 16)
+    addr = base + length - (test_len // 2)
 
-    assert (data.data != 42)
+    if os.environ["SIM"] == "icarus":  # workaround for icarus: blocks on AxiResp.SLVERR write: write to AxiRAM directly instead
+        if addr < 2**15:
+            tb.axi_ram.write(addr, test_data)
+    else:
+        res = await tb.axi_master.write(addr, test_data)
+        assert res.resp == AxiResp.SLVERR
+
+    data = await tb.axi_master.read(addr, test_len)
+    assert data.data != test_data
+    assert data.resp == AxiResp.SLVERR
+
+
+
+async def run_test_prio(dut, base: int = 0, length: int = 64):
+    """
+     Test the PMP priority scheme (i.e. port 0 (max) to port 15 (min)) by stacking a fixed memory region on top of each
+     other, once locked and once unlocked
+    """
+    # get testbed instance & reset
+    tb = TB(dut)
+    await tb.cycle_reset()
+
+    # define r/w and no access
+    access_rw = PMPAccess.ACCESS_READ.value | PMPAccess.ACCESS_WRITE.value
+    access_none = PMPAccess.ACCESS_NONE.value
+
+    # loop over all slots
+    PMP_NUM = tb.dut.axi_io_pmp0.NR_ENTRIES.value
+    lock = False
+    for i in reversed(range(PMP_NUM)):
+        # write config
+        if lock:
+            await set_pmp_napot(tb, base, length, access_none, i)
+        else:
+            await set_pmp_napot(tb, base, length, access_rw, i)
+
+        # read from configured range
+        res = await tb.axi_master.read(base, length)
+
+        # check locked/unlocked
+        if lock:
+            assert res.resp == AxiResp.SLVERR
+        else:
+            assert res.resp == AxiResp.OKAY
+
+        # toggle for next layer
+        lock = not lock
+
+
+async def run_test_granularity(dut):
+    """
+     Extract granularity according to RISC-V privileged specs
+     (1) set PMP OFF
+     (2) set all address bits
+     (3) read back address: g = 2 + NUM_LSB_ZERO_BITS
+    """
+    # get testbed instance & reset
+    tb = TB(dut)
+    await tb.cycle_reset()
+
+    # setup PMP config
+    locked = bitarray("0")
+    reserved = bitarray("00")
+    mode = PMPMode.OFF.value
+    access = PMPAccess.ACCESS_NONE.value
+    conf: bitarray = locked + reserved + mode + access
+    await tb.axi_pmp_cfg.write(16 * 8, ba2int(conf).to_bytes(8, byteorder="little"))
+
+    # write & read address
+    await tb.axi_pmp_cfg.write(0, ((2 ** 64) - 1).to_bytes(8, byteorder="little"))
+    res = await tb.axi_pmp_cfg.read(0, 8)
+
+    # compute granularity: 2 + NUM_LSB_ZERO_BITS i.e. 0b101010000 -> g=2+4=6
+    g = 0
+    while int.from_bytes(res.data, byteorder="little") & (0x1 << g) == 0:
+        g = g + 1
+    g = g + 2
+    tb.log.info(f"IO-PMP granularity: {g}")
+
+    # should be g=12 (i.e. 2^12=4K)
+    assert g == 12
+
+
+def cycle_pause():
+    return itertools.cycle([1, 1, 1, 0])
 
 
 if cocotb.SIM_NAME:
-
+    """
+    TODO
+    """
+    # define and extract bus information
     data_width = len(cocotb.top.s_axi_wdata)
     byte_lanes = data_width // 8
     max_burst_size = (byte_lanes - 1).bit_length()
+    mem_range = (0, 2 ** 16)
 
-    for test in [run_test]:
+    # run PMP tests
+    for test in [ run_test_bounds ]:
         factory = TestFactory(test)
-        # factory.add_option("size", [None] + list(range(max_burst_size)))
+        factory.add_option("length", [2**x for x in range(12, 56-1, 1)]) # test from minimal to maximal (2**56 cannot be tested with this test, since we test out-of-bound, which is not a valid physical address anymore)
+        factory.generate_tests()
+
+    for test in [ run_test_granularity, run_test_prio ]:
+        factory = TestFactory(test)
+        factory.generate_tests()
+
+    # exhaustive AXI tests with minimal PMP rules (allow / block all)
+    for test in [run_test_write_read]:
+        factory = TestFactory(test)
+        factory.add_option("length", [1, 2**12] + [random.randint(1, 2**12) for _ in range(2)]) # test boundaries + 2 random lengths
+        factory.add_option("size", list(range(max_burst_size)) + [max_burst_size]) # test all sizes
+        factory.add_option("block", [True, False]) # test allow and blocking
         factory.generate_tests()
 
 
-@pytest.mark.parametrize("reg_type", [1])  # [None, 0, 1, 2]
 @pytest.mark.parametrize("data_width", [64])  # [8, 16, 32, 64, 128]
 @pytest.mark.parametrize("addr_width", [64])  # [32, 64]
 @pytest.mark.parametrize("simulator", ["icarus"])  # ["icarus", "verilator", "questa"]
-def test_axi_io_pmp(request, simulator, addr_width, data_width, reg_type):
+def test_axi_io_pmp(request, simulator, addr_width, data_width):
+    """
+    TODO
+    """
     # activate for remote debugging
     if "REMOTE" in os.environ:
         import pydevd_pycharm
@@ -239,6 +377,7 @@ def test_axi_io_pmp(request, simulator, addr_width, data_width, reg_type):
     tests_dir = os.path.abspath(os.path.dirname(__file__))
     root_dir = os.path.abspath(os.path.join(tests_dir, '..'))
     src_dir = os.path.abspath(os.path.join(root_dir, 'src'))
+    os.environ["SIM"] = simulator
 
     # verilog source list
     verilog_sources = [
@@ -279,9 +418,10 @@ def test_axi_io_pmp(request, simulator, addr_width, data_width, reg_type):
         "register_interface/vendor/lowrisc_opentitan/src/prim_subreg_ext.sv",
         "register_interface/vendor/lowrisc_opentitan/src/prim_subreg_shadow.sv",
 
-        # axi connector
+        # connectors
         "connector/axi_master_connector.sv",
         "connector/axi_slave_connector.sv",
+        "connector/reg_cut.sv",
 
         # pmp sources
         "pmp/include/riscv.sv",
@@ -316,8 +456,7 @@ def test_axi_io_pmp(request, simulator, addr_width, data_width, reg_type):
         'ARUSER_ENABLE': 0,
         'ARUSER_WIDTH': 1,
         'RUSER_ENABLE': 0,
-        'RUSER_WIDTH': 1,
-        'REG_TYPE': reg_type
+        'RUSER_WIDTH': 1
     }
     extra_env = {f'PARAM_{k}': str(v) for k, v in parameters.items()}
 
